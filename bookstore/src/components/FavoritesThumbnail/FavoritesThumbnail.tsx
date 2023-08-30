db4c6f2845a7786ab302b5c1bcbf07c201366aef
Zhuk-Dmitrii <fgh298@rambler.ru>
2023-08-30 14:39:49 +0300

    added counter favorites

--- bookstore/src/components/FavoritesThumbnail/FavoritesThumbnail.tsx
+++ bookstore/src/components/FavoritesThumbnail/FavoritesThumbnail.tsx
@@ -1,11 +1,26 @@
+import { useSelector } from 'react-redux'
+import { RootState } from '../../redux/store'
 import { Link } from 'react-router-dom'
 import favoritesThumbnail from '../../images/favorites-thumbnail.png'
+import './favoritesThumbnail.scss'
 
 export function FavoritesThumbnail (): JSX.Element {
+   const { dataFavorites } = useSelector((state: RootState) => state.books)
+
+   function renderCounter () {
+      if (dataFavorites && dataFavorites.length > 0) {
+         return (
+            <span className="favorites-thumbnail__counter">{dataFavorites.length}</span>
+         )
+      }
+
+      return
+   }
+
    return (
-      <Link to='/favorites' className="favorites-thumbnail position-relative">
+      <Link to='/favorites' className="favorites-thumbnail">
          <img src={favoritesThumbnail} alt="упс" />
-         <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger text-black">10</span>
+         {renderCounter()}
       </Link>
    )
 }
