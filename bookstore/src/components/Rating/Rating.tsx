9e12bd2370daf2abafeccd9ef1feba8496d3ebe5
Zhuk-Dmitrii <fgh298@rambler.ru>
2023-08-30 15:35:42 +0300

    edited rating component

--- bookstore/src/components/Rating/Rating.tsx
+++ bookstore/src/components/Rating/Rating.tsx
@@ -2,20 +2,14 @@ import starActive from '../../images/star-active.png'
 import starDisabled from '../../images/star-disabled.png'
 import './rating.scss'
 
-export function Rating({rating}: {rating: string}): JSX.Element {
+export function Rating({ rating }: { rating: string }): JSX.Element {
    function renderRatingStar(): JSX.Element[] {
       const star = []
 
       for (let i: number = 1; i <= 5; i++) {
-         if (i <= Number(rating)) {
-            star.push(
-               <img key={i} src={starActive} />
-            )
-         } else {
-            star.push(
-               <img key={i} src={starDisabled} />
-            )
-         }
+         star.push(
+            <img key={i} src={i <= Number(rating) ? starActive : starDisabled} />
+         )
       }
 
       return star
